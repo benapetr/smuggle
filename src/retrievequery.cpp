bd3641681ff3537546bcd3cfc4517740d4370899
Petr Bena <benapetr@gmail.com>
2015-02-06 10:52:22 +0100

    updated flow

--- src/retrievequery.cpp
+++ src/retrievequery.cpp
@@ -13,6 +13,7 @@
 #include "apiquery.hpp"
 #include "apiqueryresult.hpp"
 #include "configuration.hpp"
+#include "generic.hpp"
 #include "retrievequery.hpp"
 #include "swsql.hpp"
 #include "syslog.hpp"
@@ -59,16 +60,33 @@ void RetrieveQuery::SetError(QString text)
     this->ProcessFailure();
 }
 
+// this macro makes it easy to run SQL statement and do rollback on failure
+// this one rollback if query fails with a given error reason
+#define SMUGGLE_ASSERT_ROLLBACK(query, error) if (!q->GetSite()->Datafile->ExecuteNonQuery(query))\
+                                              {\
+                                                  q->SetError(QString(error) + ": " + q->GetSite()->Datafile->LastError);\
+                                                  goto do_rollback;\
+                                              }
+
+// this one rollbacks if query with bound values fail with a given reason
+#define SMUGGLE_ASSERT_BOUND_ROLLBACK(query, error, value) delete sql;\
+                                                    sql = q->GetSite()->Datafile->ExecuteQuery_Bind(query, value);\
+                                                    if (sql->InError)\
+                                                    {\
+                                                        q->SetError(QString(error) + ": " + q->GetSite()->Datafile->LastError);\
+                                                        goto do_rollback;\
+                                                    }
+
 static void FinishText(Query *query)
 {
     ApiQuery *x = (ApiQuery*)query;
     RetrieveQuery *q = (RetrieveQuery*) x->CallbackOwner;
     ApiQueryResultNode *revision = x->GetApiQueryResult()->GetNode("rev");
-    QString revid, user, summary, timestamp, content;
+    QString revid, user, summary, timestamp, content, sid;
     QString model;
     SqlResult *sql = NULL;
-    QStringList pos;
-    QString SQL = "BEGIN;\n";
+    bool precached = false;
+    QString SQL;
     if (!revision)
     {
         q->SetError("No revision was returned by wiki");
@@ -85,23 +103,28 @@ static void FinishText(Query *query)
     timestamp = revision->GetAttribute("timestamp");
     content = revision->GetAttribute("contenformat");
     model = revision->GetAttribute("contentmodel");
+    // precache this
+    sid = QString::number(q->GetSite()->ID);
     int page_id;
     // Check if there is already this revision in DB
-    sql = q->GetSite()->Datafile->ExecuteQuery("SELECT rev_id FROM revision WHERE wiki = " + QString::number(q->GetSite()->ID) + " AND rev_id = " +
+    sql = q->GetSite()->Datafile->ExecuteQuery("SELECT rev_id, downloaded FROM revision WHERE wiki = " + sid + " AND rev_id = " +
                                                revid + ";");
     if (sql->InError)
     {
         q->SetError("Unable to select revision from revision table: " + q->GetSite()->Datafile->LastError);
         goto exit;
     }
+    // if this revision is already downloaded
     if (sql->Count() > 0)
     {
-        // we already got this revision in db
-        goto exit;
+        if (Generic::SafeBool(sql->GetRow(0).GetField(1).toInt()))
+            goto exit;
+        else
+            precached = true;
     }
     // Get information about the page from DB
     delete sql;
-    sql = q->GetSite()->Datafile->ExecuteQuery_Bind("SELECT id FROM page WHERE wiki = "  + QString::number(q->GetSite()->ID) + " AND name = ?",
+    sql = q->GetSite()->Datafile->ExecuteQuery_Bind("SELECT id FROM page WHERE wiki = "  + sid + " AND name = ?",
                                                     q->PageName());
 
     if (sql->InError)
@@ -117,45 +140,54 @@ static void FinishText(Query *query)
     }
 
     page_id = sql->GetRow(0).GetField(0).toInt();
-
     summary = revision->GetAttribute("comment");
 
+    // start transaction
+    SMUGGLE_ASSERT_ROLLBACK("BEGIN;", "Unable to start a transaction");
+
     // let's insert to text table
-    pos << revision->Value;
-    pos << summary; // summary is second parameter to that sql
-    SQL += "INSERT INTO text (wiki, revid, text, html) VALUES (" +
-                 QString::number(q->GetSite()->ID) + ", " +
-                 revid + ", ?, '');\n";
+    SQL = "INSERT INTO text (wiki, revid, text, html) VALUES (" + sid + ", " + revid + ", ?, '');";
+    SMUGGLE_ASSERT_BOUND_ROLLBACK(SQL, "Unable to insert to text table", revision->Value);
 
-    if (sql->InError)
+    if (!precached)
     {
-        q->SetError("Unable to insert into text table: " + q->GetSite()->Datafile->LastError);
-        goto exit;
+        // insert into revision table
+        SQL = "INSERT INTO revision(rev_id, rev_page, rev_text_id, rev_comment, rev_user_text, rev_timestamp, rev_minor_edit, rev_deleted, rev_len,"\
+                "rev_parent_id, rev_sha1, rev_content_model, rev_content_format, wiki, downloaded)"\
+                "VALUES (" + revid // revid
+                + ", " + QString::number(page_id) // rev_page
+                + ", last_insert_rowid(), ?, '', 0, 0, 0, 0, 0, '', '" + SwSql::Escape(model) + "', '" + SwSql::Escape(content) + "', "
+                + sid + ", 1 );";
+        SMUGGLE_ASSERT_BOUND_ROLLBACK(SQL, "Unable to insert to revision table", summary);
+    } else
+    {
+        // update the revision table
+        SQL = "UPDATE revision SET downloaded = 1, rev_text_id = last_insert_rowid(), rev_content_model = '" + SwSql::Escape(model) + "', "\
+              "rev_content_format = '" + SwSql::Escape(content) + "' WHERE wiki = " + sid + " AND rev_id = " + revid + ";";
+        SMUGGLE_ASSERT_BOUND_ROLLBACK(SQL, "Unable to update revision table", summary);
     }
 
-    SQL += "INSERT INTO revision(rev_id, rev_page, rev_text_id, rev_comment, rev_user_text, rev_timestamp, rev_minor_edit, rev_deleted, rev_len,"\
-            "rev_parent_id, rev_sha1, rev_content_model, rev_content_format, wiki, downloaded)"\
-            "VALUES (" + revid // revid
-            + ", " + QString::number(page_id) // rev_page
-            + ", last_insert_rowid(), ?, '', 0, 0, 0, 0, 0, '', '" + SwSql::Escape(model) + "', '" + SwSql::Escape(content) + "', "
-            + QString::number(q->GetSite()->ID) + ", 1 );";
-
-    // Commit and run
-    SQL += "COMMIT;";
-    delete sql;
-    sql = q->GetSite()->Datafile->ExecuteQuery_Bind(SQL, pos);
-    if (sql->InError)
-    {
-        DEBUG(SQL, 2);
-        q->SetError("Unable to run statement: " + q->GetSite()->Datafile->LastError);
-        goto exit;
-    }
-
+    // now we need to update the page table
+    SQL = "UPDATE page SET preferred_rev_id = " + revid + ", current_revision = " + revid + ", last_revision = " + revid + ", " +
+          "downloaded = 1, deleted = 0, wiki = " + sid + ", last_updated = strftime('%s','now')" +
+          " WHERE " + "id = " + QString::number(page_id) + ";";
+    SMUGGLE_ASSERT_ROLLBACK(SQL, "Unable to update the page table");
+    SMUGGLE_ASSERT_ROLLBACK("COMMIT;", "Unable to commit");
     q->Finish();
+    goto exit;
+
+    do_rollback:
+        if (!q->GetSite()->Datafile->ExecuteNonQuery("ROLLBACK;"))
+        {
+            Syslog::Logs->ErrorLog("Rollback failure: " + q->GetSite()->Datafile->LastError);
+            goto exit;
+        }
+
     exit:
-    if (sql)
-        delete sql;
-    x->DecRef();
+        // do some memory cleanup here
+        if (sql)
+            delete sql;
+        x->DecRef();
 }
 
 static void ErrorText(Query *query)
